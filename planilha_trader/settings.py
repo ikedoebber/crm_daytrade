7e81af36fdf0d23c5598fd98400d2197f8448d3b
ikedoebber <doebber23@gmail.com>
2026-05-04 10:11:34 -0300

    db

--- planilha_trader/settings.py
+++ planilha_trader/settings.py
@@ -52,8 +52,12 @@ WSGI_APPLICATION = 'planilha_trader.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': config('DB_ENGINE', default='django.db.backends.postgresql'),
+        'NAME': config('DB_NAME', default=''),
+        'USER': config('DB_USER', default=''),
+        'PASSWORD': config('DB_PASSWORD', default=''),
+        'HOST': config('DB_HOST', default='localhost'),
+        'PORT': config('DB_PORT', default='5432'),
     }
 }
 
